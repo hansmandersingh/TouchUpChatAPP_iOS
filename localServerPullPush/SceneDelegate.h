5d43a221994b0bf33d4a21fb6ceced0c3677f0fe
Hansmander <hansmandersingh1998@gmail.com>
2023-10-25 18:39:56 -0500

    Initial Commit

---
+++ localServerPullPush/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  localServerPullPush
+//
+//  Created by Hansmander Singh on 2023-10-25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ localServerPullPush/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  localServerPullPush
+//
+//  Created by Hansmander Singh on 2023-10-25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
